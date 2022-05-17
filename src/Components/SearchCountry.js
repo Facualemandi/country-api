047a8f732cdf1423b5650023d606047038b9996f
Facundo Alemandi <facuinstituto10@gmail.com>
2022-05-17 18:56:39 -0300

    Agregando versión movile

--- src/Components/SearchCountry.js
+++ src/Components/SearchCountry.js
@@ -1,16 +1,20 @@
 import React from "react";
 import "../Styles/SearchCountry.css";
+import { FcSearch } from "react-icons/fc";
 
 const SearchCountry = ({ search, searcher }) => {
   return (
     <>
       <section className="input_section">
-        <input
-          onChange={searcher}
-          value={search}
-          type={"text"}
-          placeholder="Enter country name..."
-        />
+        <section className="container_search">
+          <input
+            onChange={searcher}
+            value={search}
+            type={"text"}
+            placeholder="Enter country name..."
+          />
+          <FcSearch className="icon_search" />
+        </section>
       </section>
     </>
   );
